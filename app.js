5d53f7e3a2a14a8f870917f8581b2d6d91172f13
Dadi Atar <atardadi@gmail.com>
2016-08-15 00:26:47 +0300

    email notofication working. Before deployment

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
-var express = require('express');
+const express = require('express');
 var app = express();
 var nodemailer = require('nodemailer');
 var config = require('config');
 
+const username = config.get('email.username');
+const password = config.get('email.password');
 
-// create reusable transporter object using the default SMTP transport 
-var transporter = nodemailer.createTransport('smtps://atardadi%40gmail.com:pass@smtp.gmail.com');
- 
-// setup e-mail data with unicode symbols 
-var mailOptions = {
-    from: '"Fred Foo 👥" <foo@blurdybloop.com>', // sender address 
-    to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers 
-    subject: 'Hello ✔', // Subject line 
-    text: 'Hello world 🐴', // plaintext body 
-    html: '<b>Hello world 🐴</b>' // html body 
-};
- 
-// send mail with defined transport object 
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-
-
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport(`smtps://${username}%40gmail.com:${password}@smtp.gmail.com`);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Welcome to playalong notifier...');
 });
+
+
+app.get('/childAdded/:chordId', function (req, res) {
+  var chordId = req.params['chordId'];
+
+  console.log(`New Chord Added with Chord ID of ${chordId}`);
+
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+    from: '"Dadi Atar" <atardadi@gmail.com>', // sender address
+    to: 'atardadi@gmail.com', // list of receivers (comma separated)
+    subject: 'New Chord Added', // Subject line
+    text: `New Chord Added with Chord ID of ${chordId}`, // plaintext body
+    // html: '<b>Hello world 🐴</b>' // html body
+  };
+
+
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			return console.log(error);
+		}
+		console.log('Message sent: ' + info.response);
+	});
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
